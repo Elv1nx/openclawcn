2bddf0bc2d89c2648382cc9f04f58ccdb3244492
Elv1nx <58973614+Elv1nx@users.noreply.github.com>
2026-02-28 17:16:48 +0800

    Initial Commit

---
+++ openclawcn/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  openclawcn
+//
+//  Created by Elvinx on 28/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ openclawcn/openclawcnApp.swift
@@ -0,0 +1,17 @@
+//
+//  openclawcnApp.swift
+//  openclawcn
+//
+//  Created by Elvinx on 28/2/26.
+//
+
+import SwiftUI
+
+@main
+struct openclawcnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
